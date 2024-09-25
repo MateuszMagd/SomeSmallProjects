048c9d45294c55f7cfb153de8960ad90f83f1ec1
MatMag <240742@edu.p.lodz.p>
2024-09-25 20:27:08 +0200

    Basic database init in NumberAI

---
+++ NumberAI/main.py
@@ -0,0 +1,10 @@
+import tensorflow as tf
+import numpy as np
+
+learn = tf.contrib.learn
+
+mnist = learn.datasets.load_dataset('mnist')
+data = mnist.train.images
+labels = np.asarray(mnist.train.labels, dtype=np.int32)
+test_data = mnist.test.images
+test_labels = np.asarray(mnist.test.labels, dtype=np.int32)
